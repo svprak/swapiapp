78651980377bb53b1a57ec0ca451eaf2d0413e55
svp <svprak@gmail.com>
2020-06-29 19:05:53 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [],
+    };
+  }
 
+  componentDidMount() {
+    fetch('https://swapi.dev/api/people/')
+      .then((response) => response.json())
+      .then((data) => {
+        const names = data.results.map((n) => n.name);
+        this.setState({ data: names });
+      });
+  }
+
+  render() {
+    const name = this.state.data.map((n) => <li>{n}</li>);
+
+    return (
+      <div>
+        <ul>{name}</ul>
+      </div>
+    );
+  }
+}
 export default App;
